0f23cc77cbe351e30e147c5ddd0cb327064c2aaa
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-03-31 11:07:09 +1100

    layout changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Layout } from './components/Layout/Layout';
-import './Style.css'
-import { Hero } from './components/Hero'
-import { Header } from './components/Layout/Header';
-import { Footer } from './components/Layout/Footer';
-import { Socials } from './components/Socials';
-import { Skills } from './components/Skills';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Layout } from "./components/Layout/Layout";
+import "./Style.css";
+import { Hero } from "./components/Hero";
+import { Header } from "./components/Layout/Header";
+import { Footer } from "./components/Layout/Footer";
+import { Socials } from "./components/Socials";
+import { Skills } from "./components/Skills";
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <Socials />
-      <Skills />
-
-      <Footer />
-
-
-
+      <Layout>
+        <Hero />
+        <Socials />
+        <Skills />
+      </Layout>
     </>
   );
 }
