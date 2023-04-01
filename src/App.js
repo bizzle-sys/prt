305e967b405a256daeca8d36505abe87e5c0a3cb
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-04-01 17:48:02 +1100

    contact componenet added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import "./App.css";
 import { Layout } from "./components/Layout/Layout";
 import "./Style.css";
 import { Hero } from "./components/Hero";
-import { Header } from "./components/Layout/Header";
-import { Footer } from "./components/Layout/Footer";
 import { Socials } from "./components/Socials";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
@@ -17,6 +16,7 @@ function App() {
         <Socials />
         <Skills />
         <Projects />
+        <Contact />
       </Layout>
     </>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Contact = () => {
+  return (
+    <>
+      <div className="global-header">
+        <h1>Contact Me</h1>
+      </div>
+    </>
+  );
+};
