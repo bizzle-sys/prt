aa56eb7fa0200350d8137e58a0015c2c3d6a108e
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-04-02 10:39:02 +1000

    form styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Form } from "react-bootstrap";
 
 export const Contact = () => {
   return (
     <>
-      <div className="global-header">
-        <h1>Contact Me</h1>
+      <div className="container">
+        <Form className="">
+          <div className="global-header">
+            <h1>Contact Me</h1>
+          </div>
+          <label htmlFor="">Full Name</label>
+          <input type="text" />
+          <label htmlFor="">Email</label>
+          <input type="email" />
+          <label htmlFor="">Your Message</label>
+          <textarea name="" id="" cols="30" rows="10"></textarea>{" "}
+        </Form>
       </div>
     </>
   );
