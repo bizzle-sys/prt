1b3847bbcd3fa78f550b53aa73b4276620235da9
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-04-12 22:20:54 +1000

    style changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { Form } from "react-bootstrap";
+import { CustomInputs } from "./CustomInputs";
 
 export const Contact = () => {
+  const inputs = [
+    {
+      label: "Name",
+      fname: "Name",
+      type: "text",
+      required: true,
+    },
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      required: true,
+    },
+    {
+      label: "Your Message",
+      name: "message",
+      type: "text",
+    },
+  ];
   return (
     <>
       <div className="container ">
-        <Form className="">
-          <div className="global-header">
-            <h1>Contact Me</h1>
-          </div>
-          <label htmlFor="">Full Name</label>
-          <input type="text" />
-          <label htmlFor="">Email</label>
-          <input type="email" />
-          <label htmlFor="">Your Message</label>
-          <textarea name="" id="" cols="20" rows="5"></textarea>{" "}
+        <h1 className="global-header">Contact Me</h1>
+
+        <Form
+          className=" border m-auto mb-4 rounded shadow-lg p-5"
+          style={{ width: "500px" }}>
+          {inputs.map((item, i) => (
+            <CustomInputs key={i} {...item} />
+          ))}
         </Form>
       </div>
     </>
