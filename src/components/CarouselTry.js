79b6e7920370a076be844277847d88931dd9a1f4
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-04-15 11:11:45 +1000

    minor changes

--- src/components/CarouselTry.js
+++ src/components/CarouselTry.js
@@ -1,23 +1,5 @@
 import React from "react";
-import Carousel from "react-bootstrap/Carousel";
-import { useState } from "react";
-export const CarouselTry = () => {
-  const [index, setIndex] = useState(0);
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-  return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <h1>item 1</h1>
-      </Carousel.Item>
-      <Carousel.Item>
-        <h1>item 2</h1>
-      </Carousel.Item>
-      <Carousel.Item>
-        <h1>item 3</h1>
-      </Carousel.Item>
-    </Carousel>
-  );
+export const CarouselTry = () => {
+  return;
 };
